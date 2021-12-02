3c109142a32e89ca168fd153b0c30eb72bcb0612
Johannes Heuel <johannes.heuel@cern.ch>
2021-12-02 20:09:29 +0100

    add day 2

--- 2021/aoc21/src/solutions/day02.rs
+++ 2021/aoc21/src/solutions/day02.rs
@@ -1,7 +1,55 @@
 pub fn solve(input: &str) {
-    println!("Part1: {}", solve1(input))
+    let input = parse(input);
+
+    println!("Part1: {}", solve1(&input));
+    println!("Part2: {}", solve2(&input));
 }
 
-fn solve1(input: &str) -> i32 {
-    1
+fn parse(input: &str) -> Vec<(&str, i32)> {
+    input
+        .lines()
+        .map(|x| {
+            let mut splits = x.split_ascii_whitespace();
+            let direction = splits.next().expect("Could not read direction");
+            let distance = splits.next().expect("Could not read distance");
+            let distance = distance
+                .parse::<i32>()
+                .expect(&format!("Could not parse distance to integer {}", distance));
+            (direction, distance)
+        })
+        .collect()
+}
+
+fn solve1(input: &Vec<(&str, i32)>) -> i32 {
+    let mut horizontal_position = 0;
+    let mut depth = 0;
+
+    for &(direction, distance) in input {
+        match direction {
+            "up" => depth -= distance,
+            "down" => depth += distance,
+            "forward" => horizontal_position += distance,
+            unknown => panic!("unknown command: {:?}", unknown),
+        }
+    }
+    horizontal_position * depth
+}
+
+fn solve2(input: &Vec<(&str, i32)>) -> i32 {
+    let mut horizontal_position = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for &(direction, distance) in input {
+        match direction {
+            "up" => aim -= distance,
+            "down" => aim += distance,
+            "forward" => {
+                horizontal_position += distance;
+                depth += aim * distance;
+            }
+            unknown => panic!("unknown command: {:?}", unknown),
+        }
+    }
+    horizontal_position * depth
 }
